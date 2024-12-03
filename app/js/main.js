50a96c0eeaa90f98f3b1618a2e5f3271c09662ba
chirkovv13 <patyomkin2011@mail.ru>
2024-12-03 22:22:05 +0300

    adaptive slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,32 @@
 $(function () {
+  // slider
   $(".slider__list").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
     arrows: false,
     centerMode: true,
-    centerPadding: '0px',
-    focusOnSelect:  true,
-    // autoplay: true,
+    centerPadding: "0px",
+    focusOnSelect: true,
+    autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover: true,
+
+    // адаптив slider
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 577,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
